c5a3b690202201c938eaf8c9d9bd2779fa9d1c59
Your Name <you@example.com>
2021-05-15 17:11:59 +0530

    addition code

---
+++ TestDemo/src/com/digitalbytzs/Add.java
@@ -0,0 +1,17 @@
+package com.digitalbytzs;
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first no");
+		int a=sc.nextInt();
+		System.out.println("Enter second no");
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("addition is "+c);
+	}
+
+}
